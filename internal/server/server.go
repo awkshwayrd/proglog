3e045f63944c893788fe22824d389255b53a18e7
awkshwayrd <mathur.aksh@gmail.com>
2022-01-07 21:02:06 +0530

    Created log server

---
+++ internal/server/server.go
@@ -0,0 +1,81 @@
+package server
+
+import (
+	"context"
+	api "github.com/awkshwayrd/prolog/api/v1"
+)
+
+type Config struct {
+	CommitLog CommitLog
+}
+
+type CommitLog interface {
+	Append(*api.Record) (uint64, error)
+	Read(uint642 uint64) (*api.Record, error)
+}
+
+var _ api.LogServer = (*grpcServer)(nil)
+
+type grpcServer struct {
+	api.UnimplementedLogServer
+	*Config
+}
+
+func newgrpcServer(config *Config) (srv *grpcServer, err error) {
+	srv = &grpcServer{Config: config}
+	return srv, nil
+}
+func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (*api.ProduceResponse, error) {
+	offset, err := s.CommitLog.Append(req.Record)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ProduceResponse{Offset: offset}, nil
+}
+
+func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (*api.ConsumeResponse, error) {
+	record, err := s.CommitLog.Read(req.Offset)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ConsumeResponse{Record: record}, nil
+}
+
+func (s *grpcServer) ConsumeStream(req *api.ConsumeRequest, stream api.Log_ConsumeStreamServer) error {
+	for {
+		select {
+		case <-stream.Context().Done():
+			return nil
+		default:
+			res, err := s.Consume(stream.Context(), req)
+			switch err.(type) {
+			case nil:
+			case api.ErrOffsetOutOfRange:
+				continue
+			default:
+				return err
+			}
+			if err = stream.Send(res); err != nil {
+				return err
+			}
+			req.Offset++
+		}
+	}
+}
+
+func (s *grpcServer) ProduceStream(req *api.ProduceRequest, stream api.Log_ProduceStreamServer) error {
+	for {
+		err := stream.RecvMsg(req.Record)
+		if err != nil {
+			return err
+		}
+		res, err := s.Produce(stream.Context(), req)
+		if err != nil {
+			return err
+		}
+		if err = stream.Send(res); err != nil {
+			return err
+		}
+
+	}
+}
